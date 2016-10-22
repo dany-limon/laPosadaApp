4cb27687634a0cab92642c092581bdfdbed2fdfd
daniel almería <daniel.almeria@clouddistrict.com>
2016-10-22 17:46:13 +0200

    Componentes App y LaPosadaApp añadidos

---
+++ LaPosadaApp/react-native/App.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React, { Component, } from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import LaPosadaApp from './LaPosadaApp'
+
+export default class App extends Component {
+
+  constructor(props) {
+      super(props);
+    }
+
+    render() {
+      return (
+        <LaPosadaApp />
+      )
+    }
+}
